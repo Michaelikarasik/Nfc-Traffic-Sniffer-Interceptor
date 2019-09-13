e6948635c67c4887a4a78177e6b7f8df228037e5
Michaelikarasik <michaelikarasik@gmail.com>
2019-09-13 18:51:27 +0300

    converted Utils in readerApp to Java

---
+++ readerApp/src/main/java/karasik/co/nfcreader/Utils.java
@@ -0,0 +1,47 @@
+package karasik.co.nfcreader;
+
+class Utils {
+
+    private static String HEX_CHARS = "0123456789ABCDEF";
+
+    /**
+     * Convert a hex number string to a byte array
+     * @param data A hex number string
+     * @return The hex numbers as a byte array
+     */
+    static byte[] hexStringToByteArray(String data){
+
+        byte[] result = new byte[data.length() / 2];
+
+        for (int i = 0; i < data.length(); i += 2) {
+            int firstIndex = HEX_CHARS.indexOf(data.charAt(i));
+            int secondIndex = HEX_CHARS.indexOf(data.charAt(i + 1));
+
+            int octet = firstIndex << 4 | secondIndex;
+            result[i >> 1] = (byte) octet;
+        }
+
+        return result;
+    }
+
+    private static char[] HEX_CHARS_ARRAY = "0123456789ABCDEF".toCharArray();
+
+    /**
+     * Convert a byte array to one hex string
+     * @param byteArray A byte array
+     * @return A string of all the bytes in byteArray as hex
+     */
+    static String toHex(byte[] byteArray){
+        StringBuffer result = new StringBuffer();
+
+        for(byte curByte : byteArray){
+            int octet = curByte;
+            int firstIndex = (octet & 0xF0) >>> 4;
+            int secondIndex = octet & 0x0F;
+            result.append(HEX_CHARS_ARRAY[firstIndex]);
+            result.append(HEX_CHARS_ARRAY[secondIndex]);
+        }
+
+        return result.toString();
+    }
+}
